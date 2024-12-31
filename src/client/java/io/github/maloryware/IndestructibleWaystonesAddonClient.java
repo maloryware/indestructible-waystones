7f9ec6b3cc2339e0ba5eff6bb3fcc24d8ab32945
Mal Larsson-Reyes <139512944+maloryware@users.noreply.github.com>
2024-12-31 19:33:11 +0000

    Add files via upload

---
+++ src/client/java/io/github/maloryware/IndestructibleWaystonesAddonClient.java
@@ -0,0 +1,10 @@
+package io.github.maloryware;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IndestructibleWaystonesAddonClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
